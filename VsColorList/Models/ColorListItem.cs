23d320d297f4acc742a0d721191efc5cefb02109
Samir L. Boulema <sboulema@gmail.com>
2017-12-19 11:22:58 +0100

    Initial version

---
+++ VsColorList/Models/ColorListItem.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Drawing;
+
+namespace VsColorList.Models
+{
+    public class ColorListItem
+    {
+        public string Name;
+        public ThemeResourceKey Key;
+        public ThemeResourceKeyType KeyType;
+        public Guid Category;
+        public Color LightColor;
+        public Color DarkColor;
+        public Color BlueColor;
+    }
+}
